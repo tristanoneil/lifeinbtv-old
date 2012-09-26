bd40b4256fca0d42490abcba79095a41fd600040
Tristan O'Neil <tristanoneil@gmail.com>
2012-09-26 08:34:13 -0400

    Trying to fix player js.

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,34 @@
 $(function() {
-  function addPlayerCallbacks() {
+  function addPlayerEvents() {
     var iframe = $("#vimeo")[0],
         player = $f(iframe);
 
-    player.addEvent("ready", function() {
-      player.addEvent("play", function() {
-        $("section.video").css("opacity", "1");
-        $("div.tint").show();
-      });
+    player.addEvent("play", function() {
+      $("section.video").css("opacity", "1");
+      $("div.tint").show();
+    });
 
-      player.addEvent("pause", function() {
-        $("div.tint").hide();
-      });
-
-      $("div.tint").live("click", function() {
-        player.api("pause");
-        $("div.tint").hide();
-      });
+    player.addEvent("pause", function() {
+      $("div.tint").hide();
     });
   }
+
+  $("#vimeo").ready(function() {
+    addPlayerEvents();
+  });
 
   $.pjax.defaults.timeout = 4000;
 
   $("a[data-pjax]").pjax("#main", { fragment: "#main" })
 
   $("#main").on("pjax:complete", function(e, xhr, err) {
-    addPlayerCallbacks();
+    $("#vimeo").ready(function() {
+      addPlayerEvents();
+    });
   });
 
   $("#main").on("pjax:beforeSend", function(e, xhr, err) {
     $("#main .video").html("<h3>Loading</h3>")
     $("html, body").animate({ scrollTop: 0 }, "slow");
   });
-
-  addPlayerCallbacks();
 });
